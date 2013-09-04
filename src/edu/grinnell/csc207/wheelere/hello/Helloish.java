6efdb4d45d3e1c44fffd0ec234f6ca98475024a5
EarnestWheeler <wheelere@grinnell.edu>
2013-09-04 10:49:52 -0500

    Add first version of source code.

---
+++ src/edu/grinnell/csc207/wheelere/hello/Helloish.java
@@ -0,0 +1,10 @@
+package edu.grinnell.csc207.wheelere.hello;
+
+public class Helloish {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Go to hell you convuluted monstrosity.");
+		
+	}
+}
